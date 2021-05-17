e2cb4d3a7bc49bec7b779983dea2ad6d8a846b3b
Jairo Alberto <jairoalbertosalinasprimo210@gmail.com>
2021-05-17 10:25:27 -0500

    Added the base of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
             fontFamily: {
                 secundary_text: ['Quicksand'],
             },
+            colors: {
+                gray: {
+                    dark: '#36393F',
+                },
+                blue: {
+                    burple: '#5865F2',
+                },
+                purple: {
+                    discordOld: '#7289DA',
+                    discordOld_hover: '#7B8FD4',
+                },
+                green: {
+                    light: '#57F287',
+                    hover: '#359553',
+                },
+            },
         },
     },
     variants: {
