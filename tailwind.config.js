7037ba0c7ad28d41a4728fbd77fb5cc91c23e216
Jairo Alberto <jairoalbertosalinasprimo210@gmail.com>
2021-05-27 17:58:27 -0500

    almost final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
             colors: {
                 gray: {
                     dark: '#36393F',
+                    extra_dark: '#2F3136',
+                    color_text: '#B9BBBE',
+                    color_hover: '#3B3D42',
+                    'color_dark-100': '#36393F',
+                    'color-dark-200': '#202225',
                 },
                 blue: {
                     burple: '#5865F2',
@@ -20,6 +25,9 @@ module.exports = {
                 green: {
                     light: '#57F287',
                     hover: '#359553',
+                },
+                red: {
+                    circle: '#ED4245',
                 },
             },
         },
